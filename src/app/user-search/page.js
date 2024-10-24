915603b7da4ae7592b91c88aebcbada38493aefa
Brandon Chapman <brchapman@freedomdebtrelief.com>
2024-10-24 13:41:27 -0700

    add header and footer to user search and route to correct page

--- src/app/user-search/page.js
+++ src/app/user-search/page.js
@@ -5,6 +5,7 @@ import SearchIcon from '@mui/icons-material/Search';
 import { Box, Button, TextField, Typography } from '@mui/material';
 import { useForm, Controller } from 'react-hook-form';
 import { useRouter } from 'next/navigation';
+import Layout from '../components/Layout';
 
 const removeEmptyValues = (obj) => {
   return Object.fromEntries(
@@ -28,66 +29,70 @@ const UserSearch = () => {
     setErrorMessage(''); // Clear any previous error messages
 
     // Navigate to the next page with form data as query parameters
-    router.push(`/?${new URLSearchParams(enteredData).toString()}`);
+    router.push(
+      `/visitor-session?${new URLSearchParams(enteredData).toString()}`
+    );
   };
 
   return (
-    <Box
-      display="flex"
-      flexDirection="column"
-      alignItems="center"
-      justifyContent="center"
-      minHeight="100vh"
-      padding={2}
-    >
-      <Typography variant="h4" gutterBottom>
-        Customer Journey
-      </Typography>
-      <Typography variant="subtitle1" gutterBottom>
-        Enter search data in a field below
-      </Typography>
+    <Layout>
       <Box
-        component="form"
-        onSubmit={handleSubmit(onSubmit)}
         display="flex"
         flexDirection="column"
         alignItems="center"
-        width="100%"
-        maxWidth="400px"
+        justifyContent="center"
+        minHeight="680px"
+        padding={2}
       >
-        {['visit_id', 'profile_id', 'email', 'phone'].map((fieldName) => (
-          <Controller
-            key={fieldName}
-            name={fieldName}
-            control={control}
-            defaultValue=""
-            render={({ field }) => (
-              <TextField
-                {...field}
-                label={fieldName}
-                variant="outlined"
-                margin="normal"
-                fullWidth
-              />
-            )}
-          />
-        ))}
-        {errorMessage && (
-          <Typography color="error" variant="body2" sx={{ marginTop: 1 }}>
-            {errorMessage}
-          </Typography>
-        )}
-        <Button
-          type="submit"
-          variant="contained"
-          color="primary"
-          startIcon={<SearchIcon />}
-          sx={{ marginTop: 2 }}
+        <Typography variant="h4" gutterBottom>
+          Customer Journey
+        </Typography>
+        <Typography variant="subtitle1" gutterBottom>
+          Enter search data in a field below
+        </Typography>
+        <Box
+          component="form"
+          onSubmit={handleSubmit(onSubmit)}
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          width="100%"
+          maxWidth="400px"
         >
-          Search
-        </Button>
+          {['visit_id', 'profile_id', 'email', 'phone'].map((fieldName) => (
+            <Controller
+              key={fieldName}
+              name={fieldName}
+              control={control}
+              defaultValue=""
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  label={fieldName}
+                  variant="outlined"
+                  margin="normal"
+                  fullWidth
+                />
+              )}
+            />
+          ))}
+          {errorMessage && (
+            <Typography color="error" variant="body2" sx={{ marginTop: 1 }}>
+              {errorMessage}
+            </Typography>
+          )}
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            startIcon={<SearchIcon />}
+            sx={{ marginTop: 2 }}
+          >
+            Search
+          </Button>
+        </Box>
       </Box>
-    </Box>
+    </Layout>
   );
 };
 
