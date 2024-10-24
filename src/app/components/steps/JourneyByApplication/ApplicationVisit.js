4c16cc9e9b5783a4ce65552e9bf67d5b6b1e2d81
farooq <fahmed@achieve.com>
2024-10-24 15:23:35 -0700

    customer journey page  (#6)
    * byByApplicationAndId
    
    * customer journey

---
+++ src/app/components/steps/JourneyByApplication/ApplicationVisit.js
@@ -0,0 +1,41 @@
+'use client';
+
+import React from 'react';
+import { Box, Typography, Stepper, Step, StepLabel } from '@mui/material';
+
+const ApplicationVisit = props => {
+  
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      minHeight="100vh"
+      padding={2}
+    >
+      <Typography variant="h4" gutterBottom>
+        Customer Journey By app
+      </Typography>
+     
+       <Box sx={{ maxWidth: 400 }}>
+    
+      <Stepper  orientation="vertical">
+        {props.steps.map((step, index) => (
+          <Step key={index}>
+            <StepLabel
+    
+            >
+              {step}
+            </StepLabel>
+        
+          </Step>
+        ))}
+      </Stepper>
+
+      </Box>
+    </Box>
+  );
+};
+
+export default ApplicationVisit;
--- src/app/services/dataAdaptor/dba-manager.js
+++ src/app/services/dataAdaptor/dba-manager.js
@@ -1,6 +1,6 @@
 import eventdata from './json-data/user-events.json';
 
-export const getByVisitID = visitId => eventdata.filter(d => d.ids.visit_id == visitId )
+export const getByVisitID = visitId => eventdata.filter(d =>  d.visit_id == visitId  );
 
 export const search = (visitId = null, profileId = null, email =  null, phone =  null) => {
     switch (true) {
@@ -14,7 +14,7 @@ export const search = (visitId = null, profileId = null, email =  null, phone = 
           
        case !!profileId:
         {
-            const profileFilter = eventdata.filter(d => d.ids.profile_id == profileId );
+            const profileFilter = eventdata.filter(d => d.profile_id == profileId );
             const filteredEmail = profileFilter.find(data => data.email != null)
             const filterByEmail = eventdata.filter(d => d.email == filteredEmail );
             return filterByEmail && filterByEmail.length > 0 ? filterByEmail : profileFilter; 
@@ -23,14 +23,14 @@ export const search = (visitId = null, profileId = null, email =  null, phone = 
         case !!email:
         {
           
-            const filterByEmail = eventdata.filter(d => d.form.email == email );
+            const filterByEmail = eventdata.filter(d => d.email == email );
             return filterByEmail && filterByEmail.length > 0 ? filterByEmail : profileFilter; 
         }
 
-         case !!email:
+         case !!phone:
         {
           
-            const filterByPhone = eventdata.filter(d => d.form.phone == phone );
+            const filterByPhone = eventdata.filter(d => d.phone == phone );
             return filterByPhone; 
         }
           
@@ -39,7 +39,18 @@ export const search = (visitId = null, profileId = null, email =  null, phone = 
     }
 }
 
-export const groupByApplication  = visit_id => {
-    return Object.groupBy( getByVisitID(visit_id), ({ application }) => application);
+export const groupByApplication  = visitId => {
+      const applications = getByVisitID(visitId).reduce((x, y) => {
+                (x[y.application] = x[y.application] || []).push(y);
+
+                return x;
+
+            }, {});
+
+               console.log('applications::', applications)  
+    return  Object.keys(applications);
 }
 
+export const byByApplicationAndId  = (visit_id, application) => {
+    return eventdata.filter(d => d.visit_id == visit_id && d.application == application );
+}
