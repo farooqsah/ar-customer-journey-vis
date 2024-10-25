d096acee99c852515f8f8bc7fdc9cf8eadcacd24
Jing Zuo <jzuo@achieve.com>
2024-10-25 11:02:08 -0700

    update font style

--- src/app/components/steps/JourneyByApplication/ApplicationVisit.js
+++ src/app/components/steps/JourneyByApplication/ApplicationVisit.js
@@ -1,10 +1,16 @@
-'use client';
+"use client";
 
-import React from 'react';
-import { Box, Typography, Stepper, Step, StepLabel, Button } from '@mui/material';
-import Link from 'next/link'
-const ApplicationVisit = props => {
-  
+import React from "react";
+import {
+  Box,
+  Typography,
+  Stepper,
+  Step,
+  StepLabel,
+  Button,
+} from "@mui/material";
+import Link from "next/link";
+const ApplicationVisit = (props) => {
   return (
     <Box
       display="flex"
@@ -14,27 +20,26 @@ const ApplicationVisit = props => {
       minHeight="100vh"
       padding={2}
     >
-      <Typography variant="h4" gutterBottom>
+      <Typography
+        variant="h4"
+        gutterBottom
+        fontFamily={"var(--font-ultramarine)"}
+      >
         Customer Journey By app
       </Typography>
-     
-       <Box sx={{ maxWidth: 400 }}>
-    
-      <Stepper  orientation="vertical">
-        {props.steps.map((step, index) => (
-          <Step active = {true} key={index}>
-            <StepLabel
-    
-            >
-              
-    
-              <Button href="/aggregate-visualizer" variant="contained">{step} &gt; </Button>
-            </StepLabel>
-        
-          </Step>
-        ))}
-      </Stepper>
 
+      <Box sx={{ maxWidth: 400 }}>
+        <Stepper orientation="vertical">
+          {props.steps.map((step, index) => (
+            <Step active={true} key={index}>
+              <StepLabel>
+                <Button href="/aggregate-visualizer" variant="contained">
+                  {step} &gt;{" "}
+                </Button>
+              </StepLabel>
+            </Step>
+          ))}
+        </Stepper>
       </Box>
     </Box>
   );
